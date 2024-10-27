d572c87c22c5299030cee66545e4787b3738ac2c
rootiron <ggg03062@naver.com>
2024-10-28 01:20:43 +0900

    .

--- post-script/install.sh
+++ post-script/install.sh
@@ -1,5 +1,5 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace --version '4.5.2' --values ./ingress/values.yaml
+# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace --version '4.5.2' --values ./ingress/values.yaml
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd -n argocd --create-namespace --version '6.7.2' --values ./argocd/values.yaml
